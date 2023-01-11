7b84acee3cd812ab52bc90c4f4af5bbc027ff01f
yaros <yaroslavdisiak@gmail.com>
2023-01-11 15:43:05 +0200

    ex2done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int grn,cop,form1,form2,form3;
+    cout << "Enter grn: " ;
+    cin >> grn;
+    cout << "Enter cop: ";
+    cin >> cop;
+    form1 = cop % 100;
+    form2 = cop / 100;
+    form3 = grn + form2;
+    cout << form3<<" grn."<< form1<<" cop.";
+
+
+    return 0;
+}
