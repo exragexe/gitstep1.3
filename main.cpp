f0cc5ea165ff66a33155a7515bf78a9511101bb4
yaros <yaroslavdisiak@gmail.com>
2023-01-11 15:44:53 +0200

    ex3done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 
+
+#include <iostream>
+using namespace std;
+int main() {
+    double leng, width, heigh,formula;
+
+
+    cout << "Calculation of the volume of the parallelepiped." << endl;
+    cout << "Enter external data:"<< endl;
+    cout << "\tLength (cm) -> ";
+    cin >> leng;
+    cout << "\tWidth (cm) -> ";
+    cin >> width;
+    cout << "\tHeight (cm) -> ";
+    cin >> heigh;
+    formula = leng * width * heigh;
+    cout << "Volume: " << formula;
+
+
+    return 0;
+}
