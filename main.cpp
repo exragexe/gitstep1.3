8e7f1f01db600c05907bd76a751a219a2ff376f0
yaros <yaroslavdisiak@gmail.com>
2023-01-11 15:46:41 +0200

    ex5done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
 
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    double R,formula;
+    const double Pi = 3.14;
+
+
+    cout << "enter radius -> ";
+    cin >> R;
+    formula = 4/3*Pi*pow(R,3);
+    cout << "V circle = " << formula;
+
+
+    return 0;
+}
