5776472dd37693b862d66f7b8d0fb880111beb4d
yaros <yaroslavdisiak@gmail.com>
2023-01-11 15:45:33 +0200

    ex4done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+using namespace std;
+int main() {
+    double mashtab,vidstan, formula;
+
+
+    cout << "Calculation of the distance between settlements." << endl;
+    cout << "Enter the output data:" << endl;
+    cout << "\tMap scale (number of kilometers in one centimeter) -> ";
+    cin >> mashtab;
+    cout << "\tThe distance between the points that represent settlements(cm) -> ";
+    cin >> vidstan;
+    formula = mashtab * vidstan;
+    cout << "Distance between settlements: " << formula<<" km";
+
+
+    return 0;
+}
