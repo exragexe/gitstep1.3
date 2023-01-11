57e6370b47e26747762d9051b9584ec7db4cd845
yaros <yaroslavdisiak@gmail.com>
2023-01-11 15:42:16 +0200

    ex1done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int year, result;
+
+    cout << "Enter year: ";
+    cin >> year;
+
+    result = 365 + ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)));
+
+    cout <<"in "<<year<<" year: "<< result<<" days" << endl;
+
+    return 0;
+}
